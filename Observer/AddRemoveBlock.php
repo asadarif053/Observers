ac553f0bd6a7d6fa3d3176308350124add54b01d
Asaduzzaman <asadarif053@gmail.com>
2022-04-21 13:10:47 +0600

    Explanation: Remove compare button from product view page using observer

--- Observer/AddRemoveBlock.php
+++ Observer/AddRemoveBlock.php
@@ -7,13 +7,19 @@ use Magento\Framework\Event\ObserverInterface;
 class AddRemoveBlock implements ObserverInterface{
 
     public function execute(Observer $observer){
-
+        /**
+         * in Magento\Framework\View\Layout\Builder class, when the layout block generates layout_generate_blocks_after event dispatches
+         * with the event two parameter passes full_action_name and layout, we'll fetch both here
+         */
         $layout = $observer->getLayout();
 
         if($observer->getFullActionName() == "catalog_product_view"){
-
+        /**
+         * look at the catalog_proudct_view.xml, you will find the block view.addto.compare, we are amied to remove the block
+         * using unsetElement method
+         */
             $block = $layout->getBlock("view.addto.compare");
-            if($block){
+            if($block){ // sometimes the block may be removed
                 $layout->unsetElement($block->getNameInLayout()); //view.addto.compare
             }
         }
